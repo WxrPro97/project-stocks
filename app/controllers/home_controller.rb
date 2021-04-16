fbd96a28e245e0e9657ee93f66e18220fb353133
WxrPro97 <ampesandupa@gmail.com>
2021-04-16 16:30:02 +0100

    devise and db migration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
   def index
     @api = StockQuote::Stock.new(api_key: 'pk_258efef5f099409aa33aef9fd3c56cd6')
+
+    if params[:ticker] == ''
+      @empty = 'Please enter a symbol!'
+    elsif params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+      if !@stock
+          @error = 'Stock symbol doesnt exist!'
+      end
+    end
   end
 
   def about
